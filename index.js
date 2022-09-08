f90e11b3988e4c1bb049679b12788dba717c1e16
steress <atm0sphar3zlalz@gmail.com>
2022-09-08 23:49:52 +0300

    fixed some bugs at round 9 not displaying correct result, still missing AI

--- index.js
+++ index.js
@@ -55,21 +55,23 @@ const game = (() => {
             winning();    
         };
     
-    const winning = () => {
+    function winning () {
         i = board.map((e, i) => e === 'x' ? i : []);
         q = board.map((e, q) => e === 'o' ? q : []);
         
-        winConditionX = winConditions.filter(a => a.reduce((r, x) => r + i.includes(x), 0)==3);
-        winConditionO = winConditions.filter(a => a.reduce((r, o) => r + q.includes(o), 0)==3);
-        
-        if (winConditionX.length === 1) {
+        winX = winConditions.filter(a => a.reduce((r, x) => r + i.includes(x), 0)==3);
+        winO = winConditions.filter(a => a.reduce((r, o) => r + q.includes(o), 0)==3);
+
+        if (winX.length == 1) {
             removeEvents();
             message.innerText = "Winner is X!"
-        } else if (winConditionO.length == 1) {
+        } else if (winO.length == 1) {
             removeEvents();
             message.innerText = "Winner is O!"
-        } else if (round == 9) {
-            message.innerText = "It's a Draw!"
+        } else if (winX.length == 2) {
+            message.innerText = "Winner is X"
+        } else if (round == 9 && winX.length == 0) {
+            message.innerText = "It's a Draw"
         }
     };
     
@@ -78,9 +80,9 @@ const game = (() => {
         };
 
     const playerTurn = () => {
-        activePlayer == player2 ? message.innerText = `Player X's turn` : message.innerText = `Player O's turn`
-        }
-    
+            round <= 8 && activePlayer == player2 ? message.innerText = `Player X's turn` : message.innerText = `Player O's turn`
+        };
+        
     const winConditions = [
 			[0, 1, 2],
 			[3, 4, 5],
@@ -91,16 +93,20 @@ const game = (() => {
 			[0, 4, 8],
 			[2, 4, 6],
 		];
+
     const removeClass = () => {
         Array.from(buttons).forEach((element) => element.classList.remove("x"));
         Array.from(buttons).forEach((element) => element.classList.remove("O"));
-    }
+    };
+
     const addEvents = () => {
         Array.from(buttons).forEach((element) => element.addEventListener("click", handleClick, {once : true}));
-    } 
+    };
+
     const removeEvents = () => {
         Array.from(buttons).forEach((element) => element.removeEventListener("click", handleClick, {once : true}));
-    }
+    };
+
     const restart = () => {
             removeEvents();
             removeClass();
